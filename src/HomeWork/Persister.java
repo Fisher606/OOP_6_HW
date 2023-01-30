756a1cb14ff839a66d9fadbac126c40a37480fe3
Vadim <fishermanvadik@gmail.com>
2023-01-31 00:42:58 +0300

    HW

---
+++ src/HomeWork/Persister.java
@@ -0,0 +1,14 @@
+package HomeWork;
+
+public class Persister implements Saveable {
+    private User user;
+
+    public Persister(User user){
+        this.user = user;
+    }
+
+    @Override
+    public void save() {
+        System.out.println("Save user: " + user.getName());
+    }
+}
---
+++ src/HomeWork/Reportable.java
@@ -0,0 +1,5 @@
+package HomeWork;
+
+public interface Reportable {
+    String report();
+}
