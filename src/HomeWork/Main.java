ccb177b3749a69bb1f9811396e5aca7dec5d0aad
Vadim <fishermanvadik@gmail.com>
2023-01-31 00:45:45 +0300

    Correct

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -2,7 +2,7 @@ package HomeWork;
 
 public class Main{
     public static void main(String[] args){
-        User user = new User("Bob");
+        User user = new User("Abdula");
         user.report();
         user.save();
         Persister persister = new Persister(user);
